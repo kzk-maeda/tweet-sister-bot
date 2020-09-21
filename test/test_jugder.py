a41a73a118483529a02cdbe2f0b40d7083cab338
kzk-maeda <kzk.maeda0711@gmail.com>
2020-09-21 13:29:20 +0900

    create black list judger and test code

--- test/test_jugder.py
+++ test/test_jugder.py
@@ -4,20 +4,25 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 
 from judger import Judger
+from dummy_blacklists import DummyBlacklists
 
+THRETHOLD = 30000
 
 def test_tweet_volume_equals_threshold():
     trend = {
         'name': 'dummy',
-        'tweet_volume': 30000 
+        'tweet_volume': THRETHOLD 
     }
     judger = Judger()
+    judger.blacklists = DummyBlacklists()
     assert not judger.judge_whether_tweet(trend)
+    del judger
 
 def test_tweet_volume_lager_than_threshold():
     trend = {
         'name' : 'dummy',
-        'tweet_volume' : 30001
+        'tweet_volume' : THRETHOLD + 1
     }
     judger = Judger()
+    judger.blacklists = DummyBlacklists()
     assert judger.judge_whether_tweet(trend)
